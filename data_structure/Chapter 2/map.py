31f9c27a09915b2a489664ca4bf2bf07dfa92350
my-blog.github.io <wxd963996380@gmail.com>
2019-05-18 15:53:55 +0800

    xmind, ord, map #important

--- data_structure/Chapter 2/map.py
+++ data_structure/Chapter 2/map.py
@@ -1,8 +1,6 @@
-# map function return a list of the results after applying the given function
+# map function return a iterable after applying the given function
 # to each item of a given iterable(lis,tuple etc)
-# The returned value from map() (map object) then 
-# can be passed to functions like list() (to create a list), set() (to create a set) and so on.
-# map(func, iter)
+# map(function, iterable)
 
 numbers = (1,2,3,4)
 
@@ -13,3 +11,10 @@ print(list(result))
 
 for i in map(lambda x : x+x, numbers):
     print(i)
+
+# 下面是很重要的
+
+# 关于运行结果的解释：
+# <class 'map'>   :map()返回的是一个iterable,不是list等，而map object就是 iterable 所以<class 'map'>
+# {8, 2, 4, 6}    :set()的参数是iterable,所以可以把result传进去,然后迭代产生{8, 2, 4, 6},此时result已经到了末尾,类似于c++的end()
+# []: 由于result已经到了末尾，所以自然产生[]
