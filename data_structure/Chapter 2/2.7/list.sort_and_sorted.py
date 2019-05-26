ed8ef81a6841f56bb908d702e10b48693564e74a
my-blog.github.io <wxd963996380@gmail.com>
2019-05-26 21:30:06 +0800

    list.sort() and sorted()

---
+++ data_structure/Chapter 2/2.7/list.sort_and_sorted.py
@@ -0,0 +1,23 @@
+# list.sort方法会就地排序列表
+# 通:如果一个的函数或者方法对对象进行就地该动,那么将会返回None
+
+# 返回none有一个缺点,就是无法将其串联起来使用,而返回一个新对象的方法
+# 则正好相反(比如str中的所有方法)
+
+# 内置函数sorted则正好相反,它会返回一个列表
+
+# 两个函数均有的关键字
+
+# reverse 默认False
+# key 一个只要一个参数的函数,这个函数会应用于序列中的每一个元素
+
+# example
+fruits = [ 'grape', 'rasberry', 'apple', 'banana' ]
+print(sorted(fruits))
+print(fruits)
+
+print(sorted(fruits, reverse = True))
+print(sorted(fruits, key = len))
+
+fruits.sort()
+print(fruits)
